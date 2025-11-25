7a2459257956ee48532e02a878b7201fb617cf25
aunshpatel <patelaunsh28@gmail.com>
2025-11-24 23:51:01 -0500

    Add router to routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { updateFCMToken } from "../controllers/userController.js";
 
+const router = express.Router();
+
 router.post('/users/:userId/update-fcm', updateFCMToken);
 
 export default router;
