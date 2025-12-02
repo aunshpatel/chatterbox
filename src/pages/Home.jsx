43cb03073298e1d0991a33dc20dfd30322a586e5
aunshpatel <patelaunsh28@gmail.com>
2025-12-01 21:53:52 -0500

    Create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function home() {
+  return (
+    <main>
+      <div className='pb-20 z-10'>
+      {/* top */}
+        <div className='flex flex-col gap-6 p-40 px-3 max-w-6xl mx-auto'>
+          <h1 className='text-slate-900 font-bold text-3xl lg:text-5xl'>
+            Welcome to Chatterbox.
+          </h1>
+        </div>
+      </div>
+    </main>
+  )
+}
