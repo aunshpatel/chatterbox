95a36012c7c29e97911cd81e94024274f6eccdf0
Aunsh Patel <48309697+aunshpatel@users.noreply.github.com>
2026-01-07 00:01:29 -0500

    Add console.log for testing

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import User from "../models/userModel.js";
 
 export const registeredUsers = async (req, res) => {
@@ -8,10 +9,14 @@ export const registeredUsers = async (req, res) => {
       return res.status(400).json({ message: "phoneNumbers must be an array" });
     }
 
+    console.log(`phoneNumbers:${phoneNumbers}`);
+
     // Normalize phone numbers: remove spaces and optional '+' at start
     const normalizedPhones = phoneNumbers.map(num =>
       num.replace(/\s+/g, "").replace(/^\+/, "")
     );
+
+    console.log(`normalizedPhones:${normalizedPhones}`)
 
     // Fetch only users who are registered and whose normalized phoneNumber is in the list
     const registeredUsers = await User.find({
